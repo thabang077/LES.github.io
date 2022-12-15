e89690370b16bd7621b7ed08f19155dbcf4564b8
thabang077 <64046663+thabang077@users.noreply.github.com>
2022-12-15 11:23:22 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,89 +1,39 @@
-var selectedRow = null
 
-function onFormSubmit() {
-    if (validate()) {
-        var formData = readFormData();
-        if (selectedRow == null)
-            insertNewRecord(formData);
-        else
-            updateRecord(formData);
-        resetForm();
-    }
-}
+function newTableRow()
+			 {
 
-function readFormData() {
-    var formData = {};
-    formData["fullName"] = document.getElementById("fullName").value;
-    formData["empCode"] = document.getElementById("empCode").value;
-    formData["salary"] = document.getElementById("salary").value;
-    formData["city"] = document.getElementById("city").value;
-    formData["update"] = document.getElementById("update").value;
+			 
+			      var newRow= document.createElement("tr");
+				  
+				  var newNumber= document.createElement("td");
+				  var newLognumber= document.createElement("td");
+				  var newConsultant= document.createElement("td");
+				  var newUpdate= document.createElement("td");
+				  var newGroup= document.createElement("td");   
+                  isValid = true;
+				  
+				  var table= document.getElementById("tab");
+				  
+				  table.appendChild(newRow);   
+				  newRow.appendChild(newNumber);
+				  newRow.appendChild(newLognumber);
+				  newRow.appendChild(newConsultant);
+			      newRow.appendChild(newUpdate);
+				  newRow.appendChild(newGroup);  
 
-    return formData;
-}
+				  newNumber.innerHTML=document.getElementById("number").value;
+				  newLognumber.innerHTML=document.getElementById("lognumber").value;
+				  newConsultant.innerHTML=document.getElementById("consultant").value;
+                  newUpdate.innerHTML=document.getElementById("update").value;
+			      newGroup.innerHTML=document.getElementById("group").value;
 
-function insertNewRecord(data) {
-    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow(table.length);
-    cell1 = newRow.insertCell(0);
-    cell1.innerHTML = data.fullName;
-    cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.empCode;
-    cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.salary;
-    cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.city;
-    cell4 = newRow.insertCell(4);
-    cell4.innerHTML = data.update;
-    cell4 = newRow.insertCell(5);
-    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
-                       <a onClick="onDelete(this)">Delete</a>`;
-}
+			     if(document.getElementById("group").value=="invoicing")
+		        newRow.className = "style1";
+				
+				    if(document.getElementById("group").value=="workflow")
+		        newRow.className = "style2";
+				
+				    if(document.getElementById("group").value=="barcoding")
+		        newRow.className = "style3";
 
-function resetForm() {
-    document.getElementById("fullName").value = "";
-    document.getElementById("empCode").value = "";
-    document.getElementById("salary").value = "";
-    document.getElementById("city").value = "";
-    document.getElementById("update").value = "";
-
-    selectedRow = null;
-}
-
-function onEdit(td) {
-    selectedRow = td.parentElement.parentElement;
-    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("empCode").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("city").value = selectedRow.cells[3].innerHTML;
-    document.getElementById("up").value = selectedRow.cells[4].innerHTML;
-
-}
-function updateRecord(formData) {
-    selectedRow.cells[0].innerHTML = formData.fullName;
-    selectedRow.cells[1].innerHTML = formData.empCode;
-    selectedRow.cells[2].innerHTML = formData.salary;
-    selectedRow.cells[3].innerHTML = formData.city;
-    selectedRow.cells[4].innerHTML = formData.update;
-
-}
-
-function onDelete(td) {
-    if (confirm('Are you sure to delete this record ?')) {
-        row = td.parentElement.parentElement;
-        document.getElementById("employeeList").deleteRow(row.rowIndex);
-        resetForm();
-    }
-}
-function validate() {
-    isValid = true;
-    if (document.getElementById("fullName").value == "") {
-        isValid = false;
-        document.getElementById("fullNameValidationError").classList.remove("hide");
-    } else {
-        isValid = true;
-        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
-            document.getElementById("fullNameValidationError").classList.add("hide");
-    }
-    return isValid;
-}
+                    }
